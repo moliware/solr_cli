4f67449f3d514affb364d6e4d9beb568fcaade2d
moliware <miguel@moliware.com>
2012-05-22 20:36:13 +0200

    initial commit

---
+++ solr-cli.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+solr-cli
+~~~~~~~~
+
+
+
+"""
+import cmd
+import json
+import mysolr
+
+from pygments import highlight
+from pygments.formatters import TerminalFormatter
+from pygments.lexers import JavascriptLexer
+from urlparse import parse_qs
+
+
+class SolrCLI(cmd.Cmd):
+    """ """
+
+    def __init__(self):
+        # Old style class :\
+        cmd.Cmd.__init__(self)
+        self.solr = None
+        self.prompt = '(disconnected)$ '
+
+    def do_connect(self, host):
+        self.solr = mysolr.Solr(host)
+        self.prompt = '(%s)$ ' % host
+    
+    def do_query(self, query):
+        response = self.solr.search(q=query)
+        print self.__highlight(response.raw_response)
+
+    def do_uri(self, uri):
+        response = self.solr.search(**parse_qs(uri))
+        print self.__highlight(response.raw_response)
+
+    def do_EOF(self, line):
+        return True
+
+    def __highlight(self, data):
+        formatted = json.dumps(data, indent=4)
+        return highlight(formatted, formatter=TerminalFormatter(),
+                         lexer=JavascriptLexer()).rstrip()
+
+if __name__ == '__main__':
+    SolrCLI().cmdloop()
