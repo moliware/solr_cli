076b14ebb25764454bfc0efd094d03ac2f03e976
moliware <miguel@moliware.com>
2012-09-20 20:48:54 +0200

    Removes cmd dependecy and moves to readline

--- solr_cli.py
+++ solr_cli.py
@@ -7,13 +7,16 @@ solr_cli
 Command line client for solr. 
 
 """
-import cmd
+import atexit
 import json
 import mysolr
 import re
+import readline
 import signal
 import sys
 
+from blessings import Terminal
+from os.path import expanduser
 from pygments import highlight
 from pygments.formatters import TerminalFormatter
 from pygments.lexers import JavascriptLexer, XmlLexer
@@ -23,27 +26,112 @@ from urlparse import parse_qs
 __version__ = '0.2'
 
 
-class SolrCLI(cmd.Cmd):
+class SolrCLI(object):
     """ """
 
-    def __init__(self, host=None):
-        # Old style class :\
-        cmd.Cmd.__init__(self)
+    require_connexion = (
+        'ping', 'query', 'uri', 'commit', 'delete', 'optimize', 'schema',
+        'fields'
+    )
+
+    def __init__(self, host=None, history_file=expanduser("~/.solr_cli")):
         self.solr = None
-        self.prompt = '(disconnected)$ '
         self.connected = False
-        if host:
-            self.do_connect(host)
+        self.term = Terminal()
+        self.host = host
+        self.history_file = history_file
+
+        # Load history file
+        try:
+            readline.read_history_file(history_file)
+        except IOError:
+            pass
+        atexit.register(self.__save_history)
+
         # Alias
         self.do_EOF = self.do_exit = self.do_quit
 
-    def precmd(self, line):
-        if self.connected:
-            return line
-        elif not line.startswith('connect '):
-            print 'Connect to a solr server first'
-            return ''
-        return line
+        # Registers tab for autocomplete
+        if 'libedit' in readline.__doc__:
+            readline.parse_and_bind("bind '\t' rl_complete")
+        else:
+            readline.parse_and_bind("tab: complete")
+        readline.set_completer(self.__completer)
+
+        if host:
+            self.do_connect(host)
+
+        self.valid_commands = self.__get_commands()
+
+    @property
+    def prompt(self):
+        connexion_name = self.host if self.host else 'disconnected'
+        return '(%s)$ ' % connexion_name
+
+    def __completer(self, text, state):
+        response = None
+        if state == 0:
+            if text:
+                self.matches = [s 
+                                for s in self.valid_commands
+                                if s and s.startswith(text)]
+            else:
+                self.matches = self.valid_commands[:]
+
+        try:
+            response = self.matches[state]
+        except IndexError:
+            response = None
+
+        return response
+
+    def __get_commands(self):
+        attributes = dir(self)
+        commands = [attr[3:] for attr in attributes if attr.startswith('do_')]
+        return sorted(commands)
+
+    def __highlight(self, data):
+        formatted = json.dumps(data, indent=4)
+        return highlight(formatted, formatter=TerminalFormatter(),
+                         lexer=JavascriptLexer()).rstrip()
+
+    def __parse_line(self, line):
+        index = line.find(' ')
+        command = line
+        tail = None
+        if index > 0:
+            command = line[:index]
+            tail = line[index + 1:]
+        return command, tail
+
+    def __style_prompt(self):
+        style = self.term.bold_green if self.connected else self.term.bold_red
+        return style(self.prompt)
+
+    def __save_history(self):
+        readline.write_history_file(self.history_file)
+
+    def loop(self):
+        end = False
+        while not end:
+            try:
+                line = raw_input(self.__style_prompt())
+            except EOFError:
+                line = 'EOF'
+            line = line.strip()
+            if not line:
+                continue
+            command, tail = self.__parse_line(line)
+            if command in self.valid_commands:
+                if self.connected or command not in self.require_connexion:
+                    getattr(self, 'do_' + command)(tail)
+                elif not self.connected and command in self.require_connexion:
+                    print self.term.red('Connect to a solr server first')
+            else:
+                print self.term.red('Invalid command. Type help.')
+
+    def help(self):
+        pass
 
     def do_connect(self, host):
         """connect <solr_url>
@@ -55,7 +143,7 @@ class SolrCLI(cmd.Cmd):
         if host:
             self.solr = mysolr.Solr(host)
             if self.solr.is_up():
-                self.prompt = '(%s)$ ' % host
+                self.host = host
                 self.connected = True
             else:
                 print 'Cant\'t connect to %s' % host
@@ -113,7 +201,7 @@ class SolrCLI(cmd.Cmd):
         exit from the command line.
         """
         print 'Bye'
-        return True
+        exit(0)
 
     def do_commit(self, line):
         """commit
@@ -176,15 +264,10 @@ class SolrCLI(cmd.Cmd):
         print highlight(search.group(0), formatter=TerminalFormatter(),
                         lexer=XmlLexer()).rstrip()
 
-    def __highlight(self, data):
-        formatted = json.dumps(data, indent=4)
-        return highlight(formatted, formatter=TerminalFormatter(),
-                         lexer=JavascriptLexer()).rstrip()
-
 
 def main():
     host = sys.argv[1] if len(sys.argv) >= 2 else None
-    SolrCLI(host).cmdloop()
+    SolrCLI(host).loop()
 
 
 def bye(signal, frame):
